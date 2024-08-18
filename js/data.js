6035bb0032a56fea062447478fabd4ca178c02c4
foxysophiemarion <botkot1991@gmail.com>
2024-08-19 00:10:18 +0400

    complete HOMEWORK "RED COMMENT"

---
+++ js/data.js
@@ -0,0 +1,38 @@
+const feturedItems = `[
+	{
+		"image": "/img/mainpage/fetured/01.webp",
+		"title": "ELLERY X M'O CAPSULE",
+		"text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+		"price": "$52.00"
+	},
+	{
+		"image": "/img/mainpage/fetured/02.webp",
+		"title": "MANGO  PEOPLE  T-SHIRT",
+		"text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+		"price": "$52.00"
+	},
+	{
+		"image": "/img/mainpage/fetured/03.webp",
+		"title": "SUPER COLLECTION",
+		"text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+		"price": "$52.00"
+	},
+	{
+		"image": "/img/mainpage/fetured/04.webp",
+		"title": "DON'T ME NOW",
+		"text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+		"price": "$52.00"
+	},
+	{
+		"image": "/img/mainpage/fetured/05.webp",
+		"title": "BANANA",
+		"text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+		"price": "$52.00"
+	},
+	{
+		"image": "/img/mainpage/fetured/06.webp",
+		"title": "INSIDE OUT",
+		"text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+		"price": "$52.00"
+	}
+]`;
